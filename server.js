48722b7411b288c2834136f8e7a866985796f672
CodeWithAnkitSinghal <singhalankit769@gmail.com>
2018-10-09 01:04:06 +0530

    Installing handlebars.js view engine in node app and creating two new calls to display content of the hbs pages dynamically

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
-app.get('/', function(req, res){
-    res.send({
-        name: 'Ankit',
-        age: 30
-    });
-});
-
 app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'hbs');
+
+app.get('/', function(req, res){
+    res.render('home.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear(),
+        welcome: 'Welcome message'
+    })
+});
 
 //ES5 function syntax
 app.get('/about', function(req, res){
-    res.send('About Page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    })
 });
 
 //ES6 syntax of arrow function
